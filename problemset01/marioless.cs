cc14ff77c4c10c63f6fc94445db005fef53650ec
Ronica Singh <ladyinblack2010rs@gmail.com>
2025-01-10 09:09:30 +0200

    Update marioless.cs
    Modularized the application to separate the logic needed.

--- problemset01/marioless.cs
+++ problemset01/marioless.cs
@@ -1,27 +1,38 @@
 class Program {
-  public static void Main(string[] args) {
-    MarioLess();
-  }
-
   /** 
    * Towards the end of World 1-1 in Nintendo's Super Mario Bros., 
    * Mario must ascend right-aligned pyramid of bricks.
    */
-  public static void MarioLess()
-  {
-    const int bricksCount = 8;
-
-    for (int i = 1; i <= bricksCount; i++)
+  public static void Main(string[] args) {
+    // Prompt the user for the pyramid's height
+    int height;
+    do
     {
-      for (int j = 1; j <= bricksCount - i; j++)
-      {
-        Console.Write(" ");
-      }
-      for (int k = 1; k <= i; k++)
-      {
-        Console.Write("#");
-      }
-      Console.WriteLine();
+      height = Convert.ToInt32(Console.ReadLine());
     }
+    while (height < 1);
+
+    // Print a pyramid of that height
+    for (int i = 0; i < height; i++)
+    {
+      // Print row of bricks
+      print_row(height - i - 1, i + 1);
+    }
+  }
+
+  public static void print_row(int spaces, int bricks)
+  {
+    // Print spaces
+    for (int j = 0; j < spaces; j++)
+    {
+      Console.Write(" ");
+    }
+
+    // Print bricks
+    for (int k = 0; k < bricks; k++)
+    {
+      Console.Write("#");
+    }
+    Console.WriteLine();
   }
 }
